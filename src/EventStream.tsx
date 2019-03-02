fef5fa60edcccad8cdca8ad3f8e5978ae1f3a528
John Howard <howardjohn@google.com>
2019-03-01 16:31:26 -0800

    Base app

---
+++ src/EventStream.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Component } from "react";
+import { Grid } from "@material-ui/core";
+
+export class EventStream extends Component {
+
+    render() {
+        return <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            style={{ minHeight: '100vh' }}>
+            abc
+            
+        </Grid>
+    }
+}
